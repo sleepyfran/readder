506ff2c083f2983e145006772dbcd2bc67e5ace9
Fran González <fgonzaleva@icloud.com>
2019-03-05 17:12:38 +0100

    :art: Initial code structure and Webpack aliases

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@assets', path.resolve(__dirname, 'src/assets'))
+            .set('@data', path.resolve(__dirname, 'src/data'))
+            .set('@infrastructure', path.resolve(__dirname, 'src/infrastructure'))
+            .set('@view', path.resolve(__dirname, 'src/view'))
+            .set('@vuex', path.resolve(__dirname, 'src/vuex'))
+    }
+}
