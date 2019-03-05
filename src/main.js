134c83fcaf41a76ba408b2677058636d8198ba24
Fran González <fgonzaleva@icloud.com>
2019-03-05 21:58:07 +0100

    :art: Removed Prettier/Introduced new lint rules

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from "vue";
-import App from "@view/App.vue";
-import router from "./router";
-import store from "@vuex/store";
+import Vue from "vue"
+import App from "@view/App.vue"
+import router from "./router"
+import store from "@vuex/store"
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App)
+}).$mount("#app")
--- src/view/components/HelloWorld.vue
+++ src/view/components/HelloWorld.vue
@@ -96,11 +96,11 @@
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
-  }
-};
+    name: "HelloWorld",
+    props: {
+        msg: String
+    }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
