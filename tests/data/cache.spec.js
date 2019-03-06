f9b45b5f11dabbcedbd1b7c123f4957aeb8f0563
Fran González <fgonzaleva@icloud.com>
2019-03-06 02:27:22 +0100

    :art: Improving tests organization

--- tests/data/cache.spec.js
+++ tests/data/cache.spec.js
@@ -42,97 +42,107 @@ const addRandomObject = (ttl) => {
 }
 
 /* -- TESTS -- */
-test('set should add the element to the cache when it does not exist', () => {
-    const testElementWithoutTtl = addRandomObject()
-    const testElementWithTtl = addRandomObject(2000)
+describe('set', () => {
+    test('should add the element to the cache when it does not exist', () => {
+        const testElementWithoutTtl = addRandomObject()
+        const testElementWithTtl = addRandomObject(2000)
 
-    testCacheObject(testElementWithoutTtl.key, {
-        value: testElementWithoutTtl.value,
-        expireDate: testElementWithoutTtl.expireDate
+        testCacheObject(testElementWithoutTtl.key, {
+            value: testElementWithoutTtl.value,
+            expireDate: testElementWithoutTtl.expireDate
+        })
+
+        testCacheObject(testElementWithTtl.key, {
+            value: testElementWithTtl.value,
+            expireDate: testElementWithTtl.expireDate
+        })
     })
 
-    testCacheObject(testElementWithTtl.key, {
-        value: testElementWithTtl.value,
-        expireDate: testElementWithTtl.expireDate
+    test('should modify an element of the cache if the key is present', () => {
+        const testElement = addRandomObject()
+
+        testElement.value = 'modified test'
+        cache.set(testElement.key, testElement.value)
+
+        testCacheObject(testElement.key, {
+            value: testElement.value,
+            expireDate: testElement.expireDate
+        })
     })
 })
 
-test('set should modify an element of the cache if the key is present', () => {
-    const testElement = addRandomObject()
+describe('has', () => {
+    test('should return if the element is present or not', () => {
+        const testElement = addRandomObject()
 
-    testElement.value = 'modified test'
-    cache.set(testElement.key, testElement.value)
-
-    testCacheObject(testElement.key, {
-        value: testElement.value,
-        expireDate: testElement.expireDate
+        expect(cache.has(testElement.key)).toBeTruthy()
+        expect(cache.has('non-existent')).toBeFalsy()
     })
 })
 
-test('has should return if the element is present or not', () => {
-    const testElement = addRandomObject()
+describe('get', () => {
+    test('should return the element if the key is present', () => {
+        const testElement = addRandomObject()
 
-    expect(cache.has(testElement.key)).toBeTruthy()
-    expect(cache.has('non-existent')).toBeFalsy()
-})
-
-test('get should return the element if the key is present', () => {
-    const testElement = addRandomObject()
-
-    expect(cache.get(testElement.key)).toBeTruthy()
-    expect(cache.get(testElement.key)).toEqual(testElement.value)
-})
-
-test('get should return undefined if the key has expired', () => {
-    const testElement = addRandomObject(200)
-    stubTime(100)
-
-    expect(cache.get(testElement.key)).toBeUndefined()
-})
-
-test('get should return undefined if the key is not present', () => {
-    expect(cache.get('non-existent')).toBeUndefined()
-})
-
-test('remove should remove the element if they key is present', () => {
-    const testElement = addRandomObject()
-    cache.remove(testElement.key)
-
-    testCacheObject(testElement.key, undefined)
-})
-
-test('expired cache items should be removed every minute', () => {
-    jest.useFakeTimers()
-
-    cache.startExpiredCacheCheck()
-    const elementToRemove1 = addRandomObject(1000)
-    const elementToRemove2 = addRandomObject(1500)
-    const elementToKeep = addRandomObject(100000)
-
-    stubTime(300)
-    jest.runOnlyPendingTimers()
-
-    testCacheObject(elementToRemove1.key, undefined)
-    testCacheObject(elementToRemove2.key, undefined)
-    testCacheObject(elementToKeep.key, {
-        value: elementToKeep.value,
-        expireDate: elementToKeep.expireDate
+        expect(cache.get(testElement.key)).toBeTruthy()
+        expect(cache.get(testElement.key)).toEqual(testElement.value)
     })
 
-    cache.stopExpiredCacheCheck()
+    test('should return undefined if the key has expired', () => {
+        const testElement = addRandomObject(200)
+        stubTime(100)
+
+        expect(cache.get(testElement.key)).toBeUndefined()
+    })
+
+    test('should return undefined if the key is not present', () => {
+        expect(cache.get('non-existent')).toBeUndefined()
+    })
 })
 
-test('expired cache check should stop after calling stop', () => {
-    jest.useFakeTimers()
+describe('remove', () => {
+    test('should delete the element if they key is present', () => {
+        const testElement = addRandomObject()
+        cache.remove(testElement.key)
 
-    cache.startExpiredCacheCheck()
-    jest.runOnlyPendingTimers()
-    cache.stopExpiredCacheCheck()
+        testCacheObject(testElement.key, undefined)
+    })
+})
 
-    const testElement = addRandomObject(100)
+describe('expired cache check', () => {
+    test('should remove items every minute after starting', () => {
+        jest.useFakeTimers()
 
-    testCacheObject(testElement.key, {
-        value: testElement.value,
-        expireDate: testElement.expireDate
+        cache.startExpiredCacheCheck()
+        const elementToRemove1 = addRandomObject(1000)
+        const elementToRemove2 = addRandomObject(1500)
+        const elementToKeep = addRandomObject(100000)
+
+        stubTime(300)
+        jest.runOnlyPendingTimers()
+
+        testCacheObject(elementToRemove1.key, undefined)
+        testCacheObject(elementToRemove2.key, undefined)
+        testCacheObject(elementToKeep.key, {
+            value: elementToKeep.value,
+            expireDate: elementToKeep.expireDate
+        })
+
+        cache.stopExpiredCacheCheck()
+    })
+
+    test('should stop checking after calling stop', () => {
+        jest.useFakeTimers()
+
+        cache.startExpiredCacheCheck()
+        jest.runOnlyPendingTimers()
+        cache.stopExpiredCacheCheck()
+
+        const testElement = addRandomObject(100)
+
+        testCacheObject(testElement.key, {
+            value: testElement.value,
+            expireDate: testElement.expireDate
+        })
     })
 })
