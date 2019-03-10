e2ed994a647f9a797ffa225af0f1e72fb692a8d1
Fran González <fgonzaleva@icloud.com>
2019-03-10 13:27:03 +0100

    :art: Double quotes -> Single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,15 @@ module.exports = {
     env: {
         node: true
     },
-    extends: ["plugin:vue/essential"],
+    extends: ['plugin:vue/essential'],
     rules: {
-        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        indent: ["warn", 4],
-        semi: ["error", "never"]
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['warn', 4],
+        semi: ['error', 'never'],
+        quotes: [2, 'single', 'avoid-escape']
     },
     parserOptions: {
-        parser: "babel-eslint"
+        parser: 'babel-eslint'
     }
 }
--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    moduleFileExtensions: ["js", "jsx", "json", "vue"],
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
     transform: {
-        "^.+\\.vue$": "vue-jest",
-        ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
-        "^.+\\.jsx?$": "babel-jest",
-        "^.+\\.js?$": "babel-jest"
+        '^.+\\.vue$': 'vue-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        '^.+\\.jsx?$': 'babel-jest',
+        '^.+\\.js?$': 'babel-jest'
     },
     moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1",
-        "^@data/(.*)$": "<rootDir>/src/data/$1",
-        "^@infrastructure/(.*)$": "<rootDir>/src/infrastructure/$1"
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@data/(.*)$': '<rootDir>/src/data/$1',
+        '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1'
     },
-    snapshotSerializers: ["jest-serializer-vue"],
+    snapshotSerializers: ['jest-serializer-vue'],
     testMatch: [
-        "**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+        '**/tests/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
     ],
-    testURL: "http://localhost/"
+    testURL: 'http://localhost/'
 }
