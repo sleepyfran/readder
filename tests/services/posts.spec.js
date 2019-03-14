1d8099e3e218bbda339f76d555c63b7f79658d6d
Fran González <fgonzaleva@icloud.com>
2019-03-14 14:41:41 +0100

    :white_check_mark: Added posts service tests

---
+++ tests/services/posts.spec.js
@@ -0,0 +1,120 @@
+import { subredditPosts } from '@infrastructure/reddit-api'
+import { minutesToRead } from '../../src/services/reading-time'
+import posts from '@services/posts'
+
+jest.mock('@infrastructure/reddit-api', () => {
+    return {
+        ...jest.requireActual('@infrastructure/reddit-api'),
+        subredditPosts: jest.fn()
+    }
+})
+
+jest.mock('@services/reading-time', () => {
+    return {
+        ...jest.requireActual('@services/reading-time'),
+        minutesToRead: jest.fn()
+    }
+})
+
+let onSuccess
+let onError
+let onNoResults
+
+const createPost = () => {
+    return {
+        title: 'Test Title',
+        content: 'Test Content',
+        htmlContent: 'Test Content HTML',
+        url: 'Test URL'
+    }
+}
+
+describe('posts', () => {
+    beforeEach(() => {
+        onSuccess = jest.fn()
+        onError = jest.fn()
+        onNoResults = jest.fn()
+    })
+
+    test('should call onNoResults when the API returns an empty array', () => {
+        subredditPosts.mockResolvedValueOnce([])
+
+        return posts.load(
+            {
+                minutes: 5,
+                subreddit: 'test',
+            },
+            onSuccess,
+            onError,
+            onNoResults
+        ).then(_ => {
+            expect(onNoResults).toHaveBeenCalled()
+            expect(onSuccess).not.toHaveBeenCalled()
+            expect(onError).not.toHaveBeenCalled()
+        })
+    })
+
+    test('should call onNoResults when the API returns posts that surpass the specified user minutes', () => {
+        subredditPosts.mockResolvedValueOnce([
+            createPost(),
+            createPost(),
+            createPost()
+        ])
+
+        minutesToRead.mockReturnValue(5)
+
+        return posts.load(
+            {
+                minutes: 2,
+                subreddit: 'test',
+            },
+            onSuccess,
+            onError,
+            onNoResults
+        ).then(_ => {
+            expect(onNoResults).toHaveBeenCalled()
+            expect(onSuccess).not.toHaveBeenCalled()
+            expect(onError).not.toHaveBeenCalled()
+        })
+    })
+
+    test('should call onError when the API returns an error', () => {
+        const error = new Error('Test error')
+        subredditPosts.mockRejectedValue(error)
+
+        return posts.load(
+            {
+                minutes: 2,
+                subreddit: 'test',
+            },
+            onSuccess,
+            onError,
+            onNoResults
+        ).then(_ => {
+            expect(onError).toHaveBeenCalledWith(error)
+            expect(onSuccess).not.toHaveBeenCalled()
+            expect(onNoResults).not.toHaveBeenCalled()
+        })
+    })
+
+    test('should call onSuccess with randomized posts when the API returns posts that are between the specified user minutes', () => {
+        const samplePosts = [...Array(5)].map(e => createPost())
+        subredditPosts.mockResolvedValueOnce(samplePosts)
+
+        minutesToRead.mockReturnValue(1)
+
+        return posts.load(
+            {
+                minutes: 5,
+                subreddit: 'test',
+            },
+            onSuccess,
+            onError,
+            onNoResults
+        ).then(_ => {
+            expect(onSuccess).toHaveBeenCalledWith(samplePosts)
+            expect(onError).not.toHaveBeenCalled()
+            expect(onNoResults).not.toHaveBeenCalled()
+        })
+    })
+})
