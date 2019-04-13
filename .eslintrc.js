f86905fd008e85369f503d3af11f4b4a915ab5f6
Fran González <fgonzaleva@icloud.com>
2019-04-13 13:17:50 +0200

    :rotating_light: Fix linter option
    [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         node: true,
     },
     extends: ['plugin:vue/essential', 'prettier', 'prettier/vue'],
-    plugin: ['prettier'],
+    plugins: ['prettier'],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
